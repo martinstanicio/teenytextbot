e4d5080a79ab69d72cd382daf4ed3e5ec7a9df31
Martín Stanicio <martuytimo@gmail.com>
2021-03-18 12:08:03 -0300

    fix(utils/smallify): avoid uncaught exceptions

--- src/utils/smallify.ts
+++ src/utils/smallify.ts
@@ -1,41 +1,10 @@
-interface CharsetGroup {
-  uppercase: string;
-  lowercase: string;
-}
-
 export default (message: string): string => {
-  const base: CharsetGroup = {
-    uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-    lowercase: "abcdefghijklmnopqrstuvwxyz",
-  };
-  const small: CharsetGroup = {
-    uppercase: "ᴬᴮ ᴰᴱ ᴳᴴᴵᴶᴷᴸᴹᴺᴼᴾ ᴿ ᵀᵁⱽᵂ   ",
-    lowercase: "ᵃᵇᶜᵈᵉᶠᵍʰⁱʲᵏˡᵐⁿᵒᵖ ʳˢᵗᵘᵛʷˣʸᶻ",
-  };
+  const base = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  const small = "ᴬᴮ ᴰᴱ ᴳᴴᴵᴶᴷᴸᴹᴺᴼᴾ ᴿ ᵀᵁⱽᵂ   ᵃᵇᶜᵈᵉᶠᵍʰⁱʲᵏˡᵐⁿᵒᵖ ʳˢᵗᵘᵛʷˣʸᶻ";
 
   const result = message
     .split("")
-    .map(char => {
-      const baseUppercaseIndex = base.uppercase.indexOf(char);
-      const baseLowercaseIndex = base.lowercase.indexOf(char);
-
-      if (baseUppercaseIndex === -1 && baseLowercaseIndex === -1) return char;
-
-      const charset = baseUppercaseIndex !== -1 ? "uppercase" : "lowercase";
-      const auxCharset = charset === "uppercase" ? "lowercase" : "uppercase";
-
-      const index =
-        baseUppercaseIndex !== -1 ? baseUppercaseIndex : baseLowercaseIndex;
-
-      const mainReplace = small[charset][index];
-      const auxReplace = small[auxCharset][index];
-
-      return auxReplace !== " "
-        ? auxReplace
-        : mainReplace !== " "
-        ? mainReplace
-        : char;
-    })
+    .map(char => small[base.indexOf(char)]?.trim() || char)
     .join("");
 
   return result;
